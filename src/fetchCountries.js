f8c475fba80b3445b490c4a347a626a547b26b7c
rafalbarczak <rafal.barczak@intmail.pl>
2023-05-10 18:02:20 +0200

    added functionality

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fuekds=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log(error));
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+function createMarkup(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+            <p><b>Name</b>: ${country.name.official}</p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Flag</b>: <img width="200" height="100" src="${
+              country.flags.svg
+            }"></p>
+            <p><b>Languages</b>: ${Object.values(country.languages)}</p>
+            </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function searchCountry() {
+  const searchedPhrase = inputEl.value.trim();
+  if (searchedPhrase === '') {
+    countryList.innerHTML = '';
+    return;
+  }
+  fetchCountries(searchedPhrase)
+    .then(countries => createMarkup(countries))
+    .catch(error => console.log(error));
+}
+
+inputEl.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
